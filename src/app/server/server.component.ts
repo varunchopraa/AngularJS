8b17a0015e9b8d925fd040040167218d2a1012f9
Varun Chopra <31439619+varunchopraa@users.noreply.github.com>
2019-08-18 23:23:19 +0530

    Add files via upload

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-server',
+	templateUrl: './server.component.html',
+	styles: [`
+	.online {
+		color: white;
+	}
+	`]
+})
+
+export class ServerComponent{
+	serverID = 10;
+	serverStatus = 'offline';
+
+	constructor(){
+		this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+	}
+
+	getColor(){
+		return this.serverStatus === 'online' ? 'green' : 'red';
+	}
+
+	getServerStatus(){
+		return this.serverStatus;
+	}
+}
